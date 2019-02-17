77cb33e353ee7956fcfe0a5d747421c482790e31
Ly <952700304@qq.com>
2019-02-17 17:13:09 +0800

    the server code
    very easy

--- index/test.php
+++ index/test.php
@@ -3,8 +3,8 @@
 header('Access-Control-Allow-Origin:*;charset=utf-8');
 //header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 $pw = $_POST['pw'];
-if ($pw=="1314"){
-	echo "http://localhost:1314/index.html";
+if ($pw=="the enter code"){
+	echo "http://localhost:1314/index.html";//this is the file: resumeUI/index.html
 } else {
 	echo "wrong";
 }
